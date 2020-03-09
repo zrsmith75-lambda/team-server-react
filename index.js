d6127cf54ac2a2dfdb840c6f87bbf6a691a47ae9
Zac Smith <zrs3141592@gmail.com>
2020-03-09 17:45:23 -0600

    create react app

--- index.js
+++ index.js
@@ -3,6 +3,24 @@ const express = require("express");
 const server = express();
 server.use(express.json());
 
+let books = [
+  {
+    id: 1,
+    title: "vader rules",
+    year: 1975
+  },
+  {
+    id: 2,
+    title: "vader is the best boss ever",
+    year: 1977
+  },
+  {
+    id: 3,
+    title: "vader is da man!",
+    year: 1981
+  }
+];
+
 const PORT = 5632;
 
 server.listen(PORT, () => console.log(`\n server is listening on ${PORT} \n`));
